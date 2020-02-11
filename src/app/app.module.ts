c157f4a86ee07651c7facca76641b8f826c03b81
AlwinPonnan <alwin54889@gmail.com>
2020-02-11 12:39:32 +0530

    navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,17 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UserHeaderComponent } from './user/user-header/user-header.component';
+import { UserfooterComponent } from './user/userfooter/userfooter.component';
+import { Userbodysection1Component } from './user/userbodysection1/userbodysection1.component';
 
 @NgModule({
   declarations: [
-    AppComponent
-  ],
+    AppComponent,
+    UserHeaderComponent,
+    UserfooterComponent,
+    Userbodysection1Component,
+   ],
   imports: [
     BrowserModule,
     AppRoutingModule

fd2f45b74502fd305050a8b6bfb0a21d5c7d2149
abhisheksharma21551 <abhisheksharma21551@gmail.com>
2020-02-11 12:41:11 +0530

    comapare societies

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CompareSocietiesComponent } from './compare-societies/compare-societies.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CompareSocietiesComponent
   ],
   imports: [
     BrowserModule,

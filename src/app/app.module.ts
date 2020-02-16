68472ddbfe7628bd0918269a2d0aa07a4fd5afa6
AlwinPonnan <alwin54889@gmail.com>
2020-02-16 20:02:31 +0530

    ada

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,7 +7,9 @@ import { UserHeaderComponent } from './user/user-header/user-header.component';
 import { UserfooterComponent } from './user/userfooter/userfooter.component';
 import { Userbodysection1Component } from './user/userbodysection1/userbodysection1.component';
 import { CompareSocietiesComponent } from './user/compare-societies/compare-societies.component';
-import { NgImageSliderModule } from 'ng-image-slider';
+// import { NgImageSliderModule } from 'ng-image-slider';
+import { SliderComponent } from './user/slider/slider.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 @NgModule({
   declarations: [
     AppComponent,
@@ -15,11 +17,13 @@ import { NgImageSliderModule } from 'ng-image-slider';
     UserfooterComponent,
     Userbodysection1Component,
     CompareSocietiesComponent,
+    SliderComponent,
    ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NgImageSliderModule,
+    NgbModule,
+    // NgImageSliderModule,
   ],
   providers: [],
   bootstrap: [AppComponent]

4e9bdb285ad57dfcba1dfa971d080feb49e0053c
AlwinPonnan <alwin54889@gmail.com>
2020-03-18 16:24:15 +0530

    made application criteria

--- src/app/user/userapplicationcriteria/userapplicationcriteria.component.ts
+++ src/app/user/userapplicationcriteria/userapplicationcriteria.component.ts
@@ -6,10 +6,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./userapplicationcriteria.component.css']
 })
 export class UserapplicationcriteriaComponent implements OnInit {
+setShowdrop1:boolean=false;
+setShowdrop2:boolean=false;
+value="Organisation";
+arr = ["Organisation","Business","Shop"]
 
-  constructor() { }
+constructor() { }
 
   ngOnInit() {
   }
 
+  Displayshow(){
+    this.setShowdrop1=!this.setShowdrop1;
+    console.log(this.setShowdrop1+"asjkhg")
+  }
+  Displayshow2(){
+    this.setShowdrop2=!this.setShowdrop2;
+    console.log(this.setShowdrop1+"asjkhg")
+  }
+  setValue(val)
+  {
+    this.value = val;
+    this.Displayshow2()
+  }
 }
--- src/app/user/userlogin/userlogin.component.ts
+++ src/app/user/userlogin/userlogin.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { chown } from 'fs';
 
 @Component({
   selector: 'app-userlogin',
